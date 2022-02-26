a14ed2012c42978f0f3620b346a8e6359699fad3
gostevVitalii <62056973+gostevVitalii@users.noreply.github.com>
2022-02-26 17:27:11 +0300

    Lesson 21. Added Dialogs sheet

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,24 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>Dmitry</div>
+                <div className={s.dialog}>Polina</div>
+                <div className={s.dialog}>Andrey</div>
+                <div className={s.dialog}>Sveta</div>
+                <div className={s.dialog}>Paul</div>
+                <div className={s.dialog}>John</div>
+
+            </div>
+            <div className={s.messageItems}>
+                <div className={s.message}>Hi!</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>How is React-learning?</div>
+            </div>
+
+
+
         </div>
     )
 }
