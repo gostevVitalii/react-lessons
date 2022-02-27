cdae3e9cea0214dee819764a9b72b47a612a7efc
gostevVitalii <62056973+gostevVitalii@users.noreply.github.com>
2022-02-27 08:44:26 +0300

    Lesson 25. Map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -14,9 +14,9 @@ const Message = (props) => {
         <div className={s.message}>{props.message}</div>
     )
 }
-
 const Dialogs = () => {
-    let dialogsData = [
+
+    let dialogs = [
         { id: 1, name: 'Dmitry' },
         { id: 2, name: 'Polina' },
         { id: 3, name: 'Andrey' },
@@ -25,7 +25,7 @@ const Dialogs = () => {
         { id: 6, name: 'Viktor' }
     ]
 
-    let messagesData = [
+    let messages = [
         { id: 1, message: 'Hi!' },
         { id: 2, message: 'How are you?' },
         { id: 3, message: 'How is React-learning?' },
@@ -33,17 +33,17 @@ const Dialogs = () => {
         { id: 5, message: 'Yo' },
         { id: 6, message: 'Yo' }
     ]
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id} />)
+    let messagesElements = messages.map(m => <Message message={m.message} />)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+                {dialogsElements}
 
             </div>
             <div className={s.messageItems}>
-                <Message message={messagesData[0].message} />
-                <Message message={messagesData[1].message} />
-                <Message message={messagesData[2].message} />
+                {messagesElements}
             </div>
         </div>
     )
